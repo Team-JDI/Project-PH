f4aaa69631a4f87b45dfe98323022e28ec802286
Yongwoo Kim <james960506@naver.com>
2024-05-08 15:20:54 +0900

    마스터 컨트롤러 패시브부분 변경, 캐릭터스테이터스 갈아엎었습니다, 커비 특수 효과 구현 했습니다 - 따로 호출하실 필요없이 커비라면 자동으로 무기의 태그에 따라서 추가 스탯이 적용됩니다

--- js/character/WeaponController.js
+++ js/character/WeaponController.js
@@ -29,6 +29,10 @@ class WeaponController{
         if(this.characterName == "sonic") {
             this.activateSonicSpecial();
         }
+
+        if(this.characterName == "kirby") {
+            this.activateKirbySpecial();
+        }
     }
 
     update(){
@@ -317,10 +321,10 @@ class WeaponController{
                         if (effect.includes('%')) {
                             //이미 키값이 존재한다면 %를 제거해 더하고
                             if (tagStatus[status]) {
-                                tagStatus[status] += parseFloat(effect.split("%")[0]);
+                                tagStatus[status] = parseFloat(tagStatus[status].split("%")[0]) + parseFloat(effect.split("%")[0]) + "%";
                             }else {
                                 //키값이 존재하지 않는다면 추가합니다
-                                tagStatus[status] = parseFloat(effect.split("%")[0]);
+                                tagStatus[status] = effect;
                             }
                         } else {
                             //단순 숫자만 있는 경우
@@ -334,6 +338,7 @@ class WeaponController{
                 }
             }
         }
+        console.log(444444444, tagStatus);
         //캐릭터의 데이터를 리턴(updateOwnTag로 return)
         return this.scene.masterController.characterController.characterStatus.tagStatusUpdate(tagStatus);
     }
@@ -622,19 +627,96 @@ class WeaponController{
 
     //커비 스페셜
     activateKirbySpecial(weaponTag){
-        let kirbySpecialStat;
-        let updateStat;
+        let kirbySpecial = {};
         switch(true){
+            case weaponTag == "berserker" :
+                kirbySpecial = {
+                    "absorption" : 1,
+                    "attackSpeed" : 5
+                }
+                break;
+            case weaponTag == "swift" :
+                kirbySpecial = {
+                    "attackSpeed" : 5,
+                    "speed" : 5
+                }
+                break;
+            case weaponTag == "explosive" :
+                kirbySpecial = {
+                    "power" : 1
+                }
+                break;
+            case weaponTag == "fortress" :
+                kirbySpecial = {
+                    "defence" : 1,
+                    "maxHealth" : 10
+                }
+                break;
+            case weaponTag == "greed" :
+                kirbySpecial = {
+                    "luck" : 1
+                }
+                break;
+            case weaponTag == "hawkeye" :
+                kirbySpecial = {
+                    "range" : 10,
+                    "critical" : 2
+                }
+                break;
+            case weaponTag == "swordMaster" :
+                kirbySpecial = {
+                    "attackSpeed" : 5,
+                    "critical" : 2
+                }
+                break;
+            case weaponTag == "fighter" :
+                kirbySpecial = {
+                    "attackSpeed" : 5,
+                    "power" : 1
+                }
+                break;
+            case weaponTag == "assassin" :
+                kirbySpecial = {
+                    "speed" : 5,
+                    "critical" : 2,
+                    "avoidance" : 5
+                }
+                break;
+            case weaponTag == "instantlyToTheLord" :
+                kirbySpecial = {
+                    "power" : 5,
+                    "defecne" : -5
+                }
+                break;
+            case weaponTag == "soldier" :
+                kirbySpecial = {
+                    "range" : 5,
+                    "attackSpeed" : 5
+                }
+                break;
+            case weaponTag == "mechanic" :
+                kirbySpecial = {
+                    "maxHealth" : 5,
+                    "attackSpeed" : 2,
+                    "power" : 1,
+                    "range" : 2,
+                    "critical" : 1,
+                    "avoidance" : 1,
+                    "defence" : 1,
+                    "luck" : 1,
+                    "absorption" : 1
+                }
+                break;
+
             case weaponTag == "ghost" :
-                kirbySpecialStat = "avoidance";
-                updateStat = 1;
+            kirbySpecial = {
+                "avoidance" : 5,
+            }
+            break;
         }
-        this.scene.masterController.characterController.characterStatus.kirbySpecial(kirbySpecialStat, updateStat);
+        this.scene.masterController.characterController.characterStatus.activateKirbySpecialStatus(kirbySpecial);
     }
 
-    activateSonicSpecial(){
-        this.scene.masterController.characterController.characterStatus.activateSonicSpecialStatus(this.characterName);
-    }
 
     activateMechanicTag(tagCount){
         //들어왔을때 메카닉 태그가 활성화 되어있는지 안되어있는지 확인 활성화가 안되어있다면 return
